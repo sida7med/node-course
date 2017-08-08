8497f67fcb7b4dd34556bee7fd747b758399e256
Ahmed Manaa <sida7medess@gmail.com>
2017-08-08 11:54:39 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+// middleware
+app.use((req,res,next)=>{
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log+'\n',(err)=> {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+
+// app.use((req,res,next)=>{
+// 	res.render('maintenance.hbs');
+// });
+
+app.get('/',(req,res)=>{
+	res.send({
+		name:'Ahmed',
+		likes:[
+			'movies',
+			'music'
+		]
+	});
+});
+app.get('/about',(req,res)=>{
+	res.render('about.hbs');
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		err:'Unable to fulfill request'
+	});
+});
+
+app.listen(3000,()=>{
+	console.log('server is up on port 3000');
+});
